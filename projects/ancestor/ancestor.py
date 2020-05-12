436a7a1b395daf5d0b438ea3894386e37ea20caa
B Severino <biancasev@gmail.com>
2020-05-12 16:45:09 -0400

    Completed ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -33,11 +33,12 @@ def earliest_ancestor(ancestors, starting_node):
 
     for ancestor in ancestors:
         if ancestor[0] not in family_tree.vertices:
-            print(ancestor[0])
             family_tree.add_vertex(ancestor[0])
+        if ancestor[1] not in family_tree.vertices:
+            family_tree.add_vertex(ancestor[1])
 
     for relationship in ancestors:
-        family_tree.add_edge(ancestor[1], ancestor[0])
+        family_tree.add_edge(relationship[1], relationship[0])
 
     queue = Queue()
     queue.enqueue(starting_node)
@@ -48,11 +49,16 @@ def earliest_ancestor(ancestors, starting_node):
     while queue.size() > 0:
         node = queue.dequeue()
 
-        if node not in visited:
-            visited.add(node)
-            
+        if node not in visited:            
             if family_tree.get_neighbors(node):
                 for next_node in family_tree.get_neighbors(node):
                     queue.enqueue(next_node)
-            else:
+            elif visited:
                 result = node
+            visited.add(node)
+    
+    return result
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+print(earliest_ancestor(test_ancestors, 1))
