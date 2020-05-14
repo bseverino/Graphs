23e00ad9ffa451776381e580e0d4ec16e640006f
B Severino <biancasev@gmail.com>
2020-05-13 22:26:35 -0400

    Maze traversal figured out, need to optimize it for MVP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+from util import Stack
 
 # Load world
 world = World()
@@ -25,10 +26,47 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+def opposite(direction):
+    if direction == 'n':
+        return 's'
+    elif direction == 's':
+        return 'n'
+    elif direction == 'w':
+        return 'e'
+    else:
+        return 'w'
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+traversal_graph = {
+    0: {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+}
 
+while len(traversal_graph) < len(room_graph):
+    valid_directions = []
+    if '?' in traversal_graph[player.current_room.id]:
+        for x in player.current_room.get_exits():
+            if traversal_graph[player.current_room.id][x] == '?':
+                valid_directions.append(x)
+    else:
+        for x in player.current_room.get_exits():
+            if traversal_graph[player.current_room.id][x] is not None:
+                valid_directions.append(x)
+
+    direction = random.choice(valid_directions)
+    prev_room = player.current_room
+    
+    player.travel(direction)
+
+    if player.current_room == prev_room:
+        traversal_graph[player.current_room.id][direction] = None
+    else:
+        traversal_graph[prev_room.id][direction] = player.current_room.id
+        if player.current_room.id not in traversal_graph:
+            traversal_graph[player.current_room.id] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+        traversal_graph[player.current_room.id][opposite(direction)] = prev_room.id
+        traversal_path.append(direction)
 
 
 # TRAVERSAL TEST
@@ -51,12 +89,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
